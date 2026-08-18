0dbd836b3d9bb56c4cbceb6a0627551e870e40b5
Neow <samuelstivenmena@gmail.com>
2026-08-17 21:28:17 -0500

    Delete nuestra-historia directory

--- nuestra-historia/src/theme/typography.js
+++
@@ -1,7 +0,0 @@
-export const fonts = {
-  display: 'PlayfairDisplay_700Bold',
-  displayItalic: 'PlayfairDisplay_600SemiBold_Italic',
-  body: 'Nunito_400Regular',
-  bodySemi: 'Nunito_600SemiBold',
-  bodyBold: 'Nunito_800ExtraBold',
-};
